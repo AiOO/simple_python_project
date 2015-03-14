19aa1638172e91d205bb881936fdf8cfc3cd7c83
Ahn Kiwook <kiwook@outlook.com>
2015-03-14 16:44:41 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'Flask >= 0.10.1, < 1.0',
+]
+
+tests_require = [
+    'pytest >= 2.6.4, < 3.0',
+]
+
+docs_require = [
+    'Sphinx >= 1.3, < 2.0',
+]
+
+setup(
+    name='simple_python_project',
+    version='0.0.1',
+    author='Ahn Kiwook',
+    author_email='kiwook@outlook.com',
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'docs': docs_require,
+        'tests': tests_require,
+    }
+)
